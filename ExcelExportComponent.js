2498c4b115172351d13ffff4a7acfa0c7e4f7b6d
Emre Oz <50968552+emre-ozgun@users.noreply.github.com>
2022-10-20 22:40:38 +0300

    Create ExcelExportComponent.js

---
+++ ExcelExportComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FileSaver from "file-saver";
+import * as XLSX from 'xlsx';
+
+const ExcelExport = ({ csvData, fileName, wscols, isLoading, icon }) => {
+  const fileType =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  const fileExtension = ".xlsx";
+
+  const exportToCSV = (csvData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(csvData);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
+
+  return (
+    <button
+      disabled={isLoading}
+      className="btn-primary btn-outline"
+      onClick={(e) => exportToCSV(csvData, fileName, wscols)}
+    >
+      {icon}
+      <span>Excel</span>
+    </button>
+  );
+};
+
+export default ExcelExport;
